ff0baaf70123442d3c84463ad097fb62ebdb0090
jimccis1105 <jimccis1105@gmail.com>
2021-03-24 02:54:27 +0800

    implemented details page

--- src/components/details.js
+++ src/components/details.js
@@ -11,23 +11,60 @@ const Details = () => {
     },[dogId])
     return(
         <div>
-            {/* {dog[0] &&
-                <img src={dog[0].url}/>
-            } */}
-            <h2>Bread</h2>
-            <p>
-                {/* {movie.Plot} */}
-            </p>
-            <h2>Cast</h2>
-            <ul className="list-group">
-                {/* {
-                    movie.Actors && movie.Actors.split(",").map(actor =>
-                    <li className="list-group-item">
-                        {actor}
-                    </li>)
-                } */}
-            </ul>
-            {/* {JSON.stringify(dog)} */}
+            {dog[0] &&
+                <div>
+                    <img src={dog[0].url} width="400" height="400"/>
+                    <h2>
+                        Breed Data
+                    </h2>
+                    <table id="breed_data_table">
+                        <tr>
+                            <td>weight</td>
+                            <td>{dog[0]['breeds'][0]['weight']['imperial']}</td>
+                        </tr>
+                        <tr>
+                            <td>height:</td>
+                            <td>{dog[0]['breeds'][0]['height']['imperial']}</td>
+                        </tr>
+                        <tr>
+                            <td>id:</td>
+                            <td>{dog[0]['breeds'][0]['id']}</td>
+                        </tr>
+                        <tr>
+                            <td>name:</td>
+                            <td>{dog[0]['breeds'][0]['name']}</td>
+                        </tr>
+                        <tr>
+                            <td>coutnry_code:</td>
+                            <td>{dog[0]['breeds'][0]['country_code']}</td>
+                        </tr>
+                        <tr>
+                            <td>bred_for:</td>
+                            <td>{dog[0]['breeds'][0]['bred_for']}</td>
+                        </tr>
+                        <tr>
+                            <td>breed_group:</td>
+                            <td>{dog[0]['breeds'][0]['breed_group']}</td>
+                        </tr>
+                        <tr>
+                            <td>life_span:</td>
+                            <td>{dog[0]['breeds'][0]['life_span']}</td>
+                        </tr>
+                        <tr>
+                            <td>temperament:</td>
+                            <td>{dog[0]['breeds'][0]['temperament']}</td>
+                        </tr>
+                        <tr>
+                            <td>origin:</td>
+                            <td>{dog[0]['breeds'][0]['origin']}</td>
+                        </tr>
+                        <tr>
+                            <td>reference_image_id:</td>
+                            <td>{dog[0]['breeds'][0]['reference_image_id']}</td>
+                        </tr>
+                    </table>
+                </div>
+            }
         </div>
     )
 }
