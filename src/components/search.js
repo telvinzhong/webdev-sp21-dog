f88b455cf402fca765c00294e63606c07d4a3b1e
HuixianZhang <32146234+HuixianZhang@users.noreply.github.com>
2021-03-23 01:49:35 -0400

    fix the error of search screen

--- src/components/search.js
+++ src/components/search.js
@@ -45,28 +45,40 @@
 //
 // export default Search
 import React, {useState, useEffect} from 'react'
-// import movieService from '../services/dog-service'
+// import movieService from '../services/movies-service'
 import dogService from '../services/dog-service';
 import {Link, useParams, useHistory} from "react-router-dom";
 
 const Search = () => {
-    const {name} = useParams()
+    const {title} = useParams()
     const [searchName, setSearchName] = useState("")
-    const [results, setResults] = useState({Search:[]})
+    const [results, setResults] = useState([])
     const history = useHistory()
+    // console.log(history)
     useEffect(() => {
-        setSearchName(name)
-        if(name) {
-            dogService.findDogsByName(name)
+        setSearchName(title)
+        if(title) {
+            dogService.findDogsByName(title)
                 .then(results => setResults(results))
+            // console.log(setResults(results))
         }
-    }, [name])
+    }, [title])
+    // useEffect(() => {
+    //     setSearchName(searchName)
+    //     if(searchName) {
+    //         dogService.findDogsByName(searchName)
+    //             .then(results => setResults(results))
+    //     }
+    // }, [searchName])
+    // console.log("name is:", title)
     return(
         <div>
             <h1>Search</h1>
             <input
                 onChange={(event) => {
                     setSearchName(event.target.value)
+                    // console.log(event.target.value)
+                    // console.log('search name is:',searchName)
                 }}
                 className="form-control"
                 value={searchName}/>
@@ -77,7 +89,9 @@ const Search = () => {
             </button>
             <ul className="list-group">
                 {
-                    results.Search.map(dog =>
+                    // JSON.stringify(results)
+                    // JSON.stringify(Search)
+                    results.map(dog =>
                         <li className="list-group-item" key={dog.id}>
                             <Link to={`/details/${dog.id}`}>
                                 {dog.name}
