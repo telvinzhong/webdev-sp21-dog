27b25350947359a0eef96e7822e3e4bb03a235c6
HuixianZhang <32146234+HuixianZhang@users.noreply.github.com>
2021-04-25 00:58:08 -0400

    home screen responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,61 +1,47 @@
-// import React from 'react'
-// import {Link} from "react-router-dom";
-//
-// const Home = () => {
-//     return(
-//         <div>
-//             <h1>Home</h1>
-//             <Link to="/search">
-//                 Search
-//             </Link>
-//             <br/>
-//             <Link to="/details">
-//                 Detail
-//             </Link>
-//             <br/>
-//             <Link to="/login">
-//                 Login
-//             </Link>
-//         </div>
-//     )
-// }
-//
-// export default Home
 
 import React from 'react'
-import logo from './dog-home.png';
-import {Link} from "react-router-dom";
-import { Button, Navbar, Nav, Image } from 'react-bootstrap'
-import { Picture } from 'react-responsive-picture';
+// import logo from './dog-home.png';
+import logo from './home2.png';
 import NavBar from "./NavBar";
-import CommentServerTesting from './comment-server-testing'
+import {Text} from "@chakra-ui/react";
 const Home = () => {
     return(
         <div>
-            <CommentServerTesting/>
-            {/*<Navbar bg="dark" variant="dark">*/}
-            {/*    <Navbar.Brand href="#home">Pocket Dog</Navbar.Brand>*/}
-            {/*    <Nav className="mr-auto">*/}
-            {/*        <Nav.Link href="/search">Search</Nav.Link>*/}
-            {/*        <Nav.Link href="#features">Profile</Nav.Link>*/}
-            {/*        <Nav.Link href="/login">Login</Nav.Link>*/}
-            {/*        <Nav.Link href="/register">Register</Nav.Link>*/}
-            {/*    </Nav>*/}
-            {/*    /!*<Form inline>*!/*/}
-            {/*    /!*    <FormControl type="text" placeholder="Search" className="mr-sm-2" />*!/*/}
-            {/*    /!*    <Button variant="outline-info">Search</Button>*!/*/}
-            {/*    /!*</Form>*!/*/}
-            {/*</Navbar>*/}
             <NavBar/>
-            {/*<Image className="photo" src="https://image.cnbcfm.com/api/v1/image/105992231-1561667465295gettyimages-521697453.jpeg?v=1561667497&w=740&h=416"  />*/}
-            <img className="photo" src={logo} alt="logo"/>
-            {/*<Picture className="photo" src="https://image.cnbcfm.com/api/v1/image/105992231-1561667465295gettyimages-521697453.jpeg?v=1561667497&w=740&h=416"  />*/}
+            {/*<div style="margin-top:-30px;">*/}
+            {/*</div>*/}
+            {/*<img className="photo" src={logo} alt="logo"/>*/}
+            <div className="card"
+                 // style={{backgroundImage: `url("https://static9.depositphotos.com/1022647/1081/v/600/depositphotos_10813712-stock-illustration-background-animal-footprints.jpg")`}}>
+                 style={{backgroundImage: `url("https://cdn.sandbergwallpaper.com/wp-content/uploads/2020/11/237-21_1.jpg")`}}>
+                <div className="row g-0">
+                    {/*<div className="col-md-4 mt-5 mr-4 mr-sm-4 mr-md-0 pl-5">*/}
+                    <div className="col-md-4 mt-5 mr-4 mr-sm-4 mr-md-0 pl-5">
+                        <img className="photo" src={logo} alt="logo"/>
+                        {/*<img className="img float-right" src={dog[0].url}/>*/}
+
+                    </div>
+                    <div className="col-md-8 pl-5 mt-5">
+                        <div className="card-body text-center">
+
+                            <div className="card-title">
+                                {/*<h1 className="card-title h1">Hi! Welcome! Our mission is to create centralized online dog community*/}
+
+                                {/*    for both existing and potential dog owners to learn about and trade pictures of dogs</h1>*/}
+                                <Text fontSize='35px' fontWeight='bold' color='white' as='em'>The goal of this project is to create an online dog community
+                                    for both existing and potential dog owners to discover, share, and discuss all things dog related.
+                                    Users can find information on dog breeds, and interact with one another by leaving comments on the pages of their favorite dogs.
+
+                                </Text>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
-
-
-
     )
 }
 
 export default Home
+
